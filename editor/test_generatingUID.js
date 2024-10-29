7455e93bd278d93d5685ff0700ae7fb43fe7b629
Schoeneh <116974162+Schoeneh@users.noreply.github.com>
2024-10-29 16:31:17 +0100

    updated js

--- editor/test_generatingUID.js
+++ editor/test_generatingUID.js
@@ -1,8 +1,6 @@
 const ShortUniqueId = require('short-unique-id');
 const uid = new ShortUniqueId();
 
-//console.log(uid.approxMaxBeforeCollision(uid.availableUUIDs(12)))
-
 let uidWithTimestamp = uid.stamp(12);
 console.log(uidWithTimestamp);
 let recoveredTimestamp = uid.parseStamp(uidWithTimestamp);
